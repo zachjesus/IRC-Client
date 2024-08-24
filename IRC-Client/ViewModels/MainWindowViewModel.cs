6553bfb86f13fa2af879cd3cb9d9af85ccf92cde
Zach Rosario <zrosario301@gmail.com>
2024-08-23 21:29:04 -0400

    main ui + project core

---
+++ IRC-Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace IRC_Client.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    public string AppTitle => "WormChat";
+    public string Server => "Blank";
+    public string Port => "Blank";
+    public string Channel => "Blank";
+    public string Nickname => "Blank";
+    public string Password => "Blank";
+#pragma warning restore CA1822 // Mark members as static
+}
---
+++ IRC-Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace IRC_Client.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
